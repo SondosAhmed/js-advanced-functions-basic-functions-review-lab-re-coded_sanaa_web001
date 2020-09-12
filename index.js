b6c17ac4a4f4498a19fbf211f58988ab5fc03b85
Sondos Ahmed <sondosalshami@gmail.com>
2020-09-12 15:39:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-// Your code here
-function saturdayFun(name="roller-skate"){
-  return "This Saturday, I want to "+name+"!";
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork= function (arg="go to the office"){
-  return "This Monday, I will "+arg+".";
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective(msg="*") {
-  return function(name="special") {
-    return "You are "+`${name}`+msg+"*i";
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
   }
 }
 
-function wrapAdjective(msg = "*") { // 2
-    return function(name = "special") { // 3
-        return "You are " +
-            ` ${msg}${name}`; // 4
-    }
-    calculator ={
-      function add {
-        return 1+3;
-      }
-}
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
 }
 
-wrapAdjective()();
+let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
+}
