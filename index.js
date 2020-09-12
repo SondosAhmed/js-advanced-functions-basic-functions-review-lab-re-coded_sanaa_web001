4a2c11bb8c9b8118a1e28e4a71bd781228c3dd20
Sondos Ahmed <sondosalshami@gmail.com>
2020-09-12 15:26:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function saturdayFun(name="roller-skate"){
+  return "This Saturday, I want to "+name+"!";
+}
+
+let mondayWork= function (arg="go to the office"){
+  return "This Monday, I will "+arg+".";
+}
+
+function wrapAdjective(msg="*") {
+  return function(name="special") {
+    return "You are "+`${name}`+msg+"*i";
+  }
+}
+
+function wrapAdjective(msg = "*") { // 2
+    return function(name = "special") { // 3
+        return "You are " +
+            ` ${msg}${name}`; // 4
+    }
+    calculator ={
+      function add {
+        return 1+3;
+      }
+}
+}
+
+wrapAdjective()();
